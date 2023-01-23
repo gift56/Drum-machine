27017e5e17fed9b58d92be8e3150ed9981d2cf4a
gift56 <efeasiughu@gmail.com>
2023-01-23 06:58:47 -0800

    working on recording

--- src/components/Records.tsx
+++ src/components/Records.tsx
@@ -1,5 +1,15 @@
 const Records = ({ recording }: any) => {
-  return <h3>{recording}</h3>;
+  return (
+    <div className="my-5">
+      <h3>{recording}</h3>
+      {recording && (
+        <div className="flex justify-center items-center gap-5 mt-3">
+          <button className="bg-green-500 py-2 px-5 cursor-pointer rounded outline-none">Play</button>
+          <button className="bg-red-500 py-2 px-5 cursor-pointer rounded outline-none">Clear</button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Records;
