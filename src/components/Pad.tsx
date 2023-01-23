03f96deb680107919437958e6414f18facf66d67
gift56 <efeasiughu@gmail.com>
2023-01-23 06:42:44 -0800

    playing sound

--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -16,10 +16,7 @@ const Pad = ({ clip, active, setActive }: any) => {
 
   const playSound = () => {
     const audioTag: any = document.getElementById(clip.keyTrigger);
-    setActive(true);
-    setTimeout(() => {
-      setActive(false);
-    }, 100);
+
     audioTag.currentTime = 0;
     audioTag.play();
   };
@@ -27,9 +24,7 @@ const Pad = ({ clip, active, setActive }: any) => {
   return (
     <button
       onClick={playSound}
-      className={`py-4 px-8 outline-none border m-3 bg-gray-300 rounded-md cursor-pointer text-gray-800 text-lg font-medium ${
-        active && "bg-yellow-400"
-      }`}
+      className={`py-4 px-8 outline-none border m-3 bg-gray-300 rounded-md cursor-pointer text-gray-800 text-lg font-medium `}
     >
       <audio id={clip.keyTrigger} src={clip.url} className="" />
       {clip.keyTrigger}
