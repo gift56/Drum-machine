c4b7b5bfe5264ffc63a8ea98a04b5e216aee5f5b
gift56 <efeasiughu@gmail.com>
2023-01-23 12:16:02 -0800

    added some update

--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -9,7 +9,6 @@ interface Iprop {
 }
 
 const Pad = ({ clip, volume, setRecording, active, setActive }: Iprop) => {
-  console.log(clip);
   const handleKeyPress = (e: any) => {
     if (e.keyCode === clip.keyCode) {
       playSound();
@@ -26,6 +25,8 @@ const Pad = ({ clip, volume, setRecording, active, setActive }: Iprop) => {
   const playSound = () => {
     const audioTag: any = document.getElementById(clip.keyTrigger);
     audioTag.currentTime = 0;
+    setActive(true);
+    setTimeout(() => setActive(false), 100);
     audioTag.play();
     audioTag.volume = volume;
     setRecording((prev: any) => prev + clip.keyTrigger + " ");
@@ -34,7 +35,9 @@ const Pad = ({ clip, volume, setRecording, active, setActive }: Iprop) => {
   return (
     <button
       onClick={playSound}
-      className={`py-4 px-8 outline-none border m-3 bg-[#0DCCCC] border-[#0DCCCC] rounded-md cursor-pointer text-gray-800 text-lg font-medium `}
+      className={`py-4 px-8 outline-none border m-3 bg-[#0DCCCC] border-[#0DCCCC] rounded-md cursor-pointer text-gray-800 text-lg font-medium ${
+        active ? "!bg-yellow-300 border-yellow-300" : ""
+      }`}
     >
       <audio id={clip.keyTrigger} src={clip.url} />
       {clip.keyTrigger}
