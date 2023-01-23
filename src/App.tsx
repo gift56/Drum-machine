197d21d96c5b4392371920a4fbfcb52567bd2d85
gift56 <efeasiughu@gmail.com>
2023-01-23 06:07:24 -0800

    it worked

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div className="text-3xl underline">App</div>;
+};
 
-export default App
+export default App;
