aad28277c1d1b34eef00847c311f3f11ec502453
gift56 <efeasiughu@gmail.com>
2023-01-23 06:23:33 -0800

    self closing audio

--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Pad = ({ clip }: any) => {
-  return <button className="py-4 px-8 outline-none border m-3 bg-gray-300 rounded-md cursor-pointer text-gray-800 text-lg font-medium">Pad</button>;
+  return (
+    <button className="py-4 px-8 outline-none border m-3 bg-gray-300 rounded-md cursor-pointer text-gray-800 text-lg font-medium">
+      <audio />
+    </button>
+  );
 };
 
 export default Pad;
