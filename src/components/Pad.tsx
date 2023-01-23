170e61fa2549d58e0ce35ac44d10558ae60a2ab2
gift56 <efeasiughu@gmail.com>
2023-01-23 06:22:58 -0800

    started coding

--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Pad = ({ clip }: any) => {
-  return <div>Pad</div>;
+  return <button className="py-4 px-8 outline-none border m-3 bg-gray-300 rounded-md cursor-pointer text-gray-800 text-lg font-medium">Pad</button>;
 };
 
 export default Pad;
