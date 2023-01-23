880925a77d7bcee4c013e60a6f0a27dda1fe879d
gift56 <efeasiughu@gmail.com>
2023-01-23 08:26:13 -0800

    changed logo

--- src/App.tsx
+++ src/App.tsx
@@ -28,38 +28,41 @@ const App = () => {
   };
 
   return (
-    <main className="bg-[#0C131A] w-full text-white min-h-screen flex flex-col gap-5 justify-center items-center">
-      <div className="flex flex-col w-full items-center justify-center gap-4">
-        <h2 className="text-2xl font-semibold">Drum Machine</h2>
-        <div className="grid grid-cols-3 border rounded">
-          {audioSound.map((clip) => (
-            <Pad
-              clip={clip}
-              key={clip.id}
-              setActive={setActive}
-              active={active}
-              volume={volume}
-              setRecording={setRecording}
-            />
-          ))}
+    <>
+      <p><i>created by</i><a href="https://github.com/gift56">Efe Gift</a></p>
+      <main className="bg-[#0C131A] w-full text-white min-h-screen flex flex-col gap-5 justify-center items-center">
+        <div className="flex flex-col w-full items-center justify-center gap-4">
+          <h2 className="text-2xl font-semibold">Drum Machine</h2>
+          <div className="grid grid-cols-3 border rounded">
+            {audioSound.map((clip) => (
+              <Pad
+                clip={clip}
+                key={clip.id}
+                setActive={setActive}
+                active={active}
+                volume={volume}
+                setRecording={setRecording}
+              />
+            ))}
+          </div>
         </div>
-      </div>
-      <h3>{recording}</h3>
-      <div className="flex w-full items-center justify-center gap-5">
-        <Volume
-          volume={volume}
-          setVolume={setVolume}
-          speed={speed}
-          setSpeed={setSpeed}
-          recording={recording}
-        />
-        <Records
-          recording={recording}
-          setRecording={setRecording}
-          playRecording={playRecording}
-        />
-      </div>
-    </main>
+        <h3>{recording}</h3>
+        <div className="flex w-full items-center justify-center gap-5">
+          <Volume
+            volume={volume}
+            setVolume={setVolume}
+            speed={speed}
+            setSpeed={setSpeed}
+            recording={recording}
+          />
+          <Records
+            recording={recording}
+            setRecording={setRecording}
+            playRecording={playRecording}
+          />
+        </div>
+      </main>
+    </>
   );
 };
 
