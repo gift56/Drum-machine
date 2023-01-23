182e09d3297af812ec4689e42efad8688ef61cff
gift56 <efeasiughu@gmail.com>
2023-01-23 06:26:38 -0800

    creating a function

--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 const Pad = ({ clip }: any) => {
+  const playSound = () => {};
+
   return (
-    <button className="py-4 px-8 outline-none border m-3 bg-gray-300 rounded-md cursor-pointer text-gray-800 text-lg font-medium">
-      <audio />
+    <button
+      onClick={playSound}
+      className={`py-4 px-8 outline-none border m-3 bg-gray-300 rounded-md cursor-pointer text-gray-800 text-lg font-medium`}
+    >
+      <audio id={clip.keyTrigger} src={clip.url} className="" />
+      {clip.keyTrigger}
     </button>
   );
 };
